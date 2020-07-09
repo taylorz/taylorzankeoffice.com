1de97355509298181a5522a8fd9eb8beefbfb08f
taylorz <taylor.zanke@gmail.com>
2020-07-09 15:33:17 -0700

    re order credits for beijing client

--- src/constants/work.js
+++ src/constants/work.js
@@ -11,7 +11,7 @@ const WORK = [
     caption: "Scanned spatial surveys are placed in a spread, revealing scales of detail.",
     year: "2016",
     info: "Housing The Majority was completed as part of a month long studio visit to Shenzhen, China. The purpose of the trip was to investigate the rapid growth of the city and develop research pertaining to certain regions within the city. Once rural regions known now as 'Urban Villages' have been subsumed by the urban growth of the city around them causing social, cultural, and spatial tensions which may not have occured if the growth occured at a more stable rate. This book was produced to collate resarch done by students at Columbia GSAPP. The book design highlights artifacts and visual material produced by students to undertake their research, acting both as an archive of work as well as an archive of the process of working.",
-    credits: ["Yujing Mandy Han","Ellen Huang", "Ivy Huang", "Zoe He", "Valérie Lechêne", "Brigitte Kathleen Lucey", "Zedu Angus Luo", "Kevin MacNichol", "Stine Redder Pedersen", "Jasmine Sun", "Haijian Jerry Zie", "Xiaoming Xu", "Taylor Zanke"],
+    credits: ["Yujing Mandy Han","Ellen Huang", "Ivy Huang", "Zoe He", "Valérie Lechêne", "Brigitte Kathleen Lucey", "Zedu Angus Luo", "Kevin MacNichol", "Stine Redder Pedersen", "Jasmine Sun", "Xiaoming Xu", "Taylor Zanke", "Haijian Jerry Zie"],
     deliverable: "Book, 5.75 x 8.25 inches, 221 pages."
   },
   {
@@ -22,7 +22,7 @@ const WORK = [
     caption: "Notes and diagrams are presented together, a working process is revealed.",
     year: "2016",
     info: "Housing The Majority was completed as part of a month long studio visit to Shenzhen, China. The purpose of the trip was to investigate the rapid growth of the city and develop research pertaining to certain regions within the city. Once rural regions known now as 'Urban Villages' have been subsumed by the urban growth of the city around them causing social, cultural, and spatial tensions which may not have occured if the growth occured at a more stable rate. This book was produced to collate resarch done by students at Columbia GSAPP. The book design highlights artifacts and visual material produced by students to undertake their research, acting both as an archive of work as well as an archive of the process of working.",
-    credits: ["Yujing Mandy Han","Ellen Huang", "Ivy Huang", "Zoe He", "Valérie Lechêne", "Brigitte Kathleen Lucey", "Zedu Angus Luo", "Kevin MacNichol", "Stine Redder Pedersen", "Jasmine Sun", "Haijian Jerry Zie", "Xiaoming Xu", "Taylor Zanke"],
+    credits: ["Yujing Mandy Han","Ellen Huang", "Ivy Huang", "Zoe He", "Valérie Lechêne", "Brigitte Kathleen Lucey", "Zedu Angus Luo", "Kevin MacNichol", "Stine Redder Pedersen", "Jasmine Sun", "Xiaoming Xu", "Taylor Zanke", "Haijian Jerry Zie"],
     deliverable: "Book, 5.75 x 8.25 inches, 221 pages."
   },
   {
@@ -33,7 +33,7 @@ const WORK = [
     caption: "Book cover with a poster printed on-site.",
     year: "2016",
     info: "Housing The Majority was completed as part of a month long studio visit to Shenzhen, China. The purpose of the trip was to investigate the rapid growth of the city and develop research pertaining to certain regions within the city. Once rural regions known now as 'Urban Villages' have been subsumed by the urban growth of the city around them causing social, cultural, and spatial tensions which may not have occured if the growth occured at a more stable rate. This book was produced to collate resarch done by students at Columbia GSAPP. The book design highlights artifacts and visual material produced by students to undertake their research, acting both as an archive of work as well as an archive of the process of working.",
-    credits: ["Yujing Mandy Han","Ellen Huang", "Ivy Huang", "Zoe He", "Valérie Lechêne", "Brigitte Kathleen Lucey", "Zedu Angus Luo", "Kevin MacNichol", "Stine Redder Pedersen", "Jasmine Sun", "Haijian Jerry Zie", "Xiaoming Xu", "Taylor Zanke"],
+    credits: ["Yujing Mandy Han","Ellen Huang", "Ivy Huang", "Zoe He", "Valérie Lechêne", "Brigitte Kathleen Lucey", "Zedu Angus Luo", "Kevin MacNichol", "Stine Redder Pedersen", "Jasmine Sun", "Xiaoming Xu", "Taylor Zanke", "Haijian Jerry Zie"],
     deliverable: "Book, 5.75 x 8.25 inches, 221 pages."
   },
 ]
