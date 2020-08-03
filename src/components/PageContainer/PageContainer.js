dbc739ba1e414cf47f2839d4cd64e4ed0e8c1eeb
taylorz <taylor.zanke@gmail.com>
2020-08-03 14:27:13 -0700

    100vh div on mobile

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Div100vh from 'react-div-100vh'
 
 import './PageContainer.scss'
 
 const PageContainer = ({ children, className }) => (
+  <Div100vh
+    style={{
+      height: `calc(100% - 80px`
+    }}
+  >
     <Grid container xs={12} className={`page-container ${className}`}>
-      {children}
+        {children}
     </Grid>
+  </Div100vh>
 )
 
 export default PageContainer;
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,7 +6,7 @@ import PageContainer from '../../components/PageContainer/PageContainer'
 const Contact = () => (
   <PageContainer className="contact">
     <Grid container>
-      <Grid item xs={12} sm={4} className="spacer"/>
+      <Grid item xs={0} sm={4} className="spacer"/>
       <Grid item xs={12} sm={8} md={6} lg={5} xl={4} className="description">
         <p>Taylor Zanke<br/>Los Angeles, CA</p>
         <p>taylor@taylorzankeoffice.com<br/>+ 1 917 826 7277</p>
